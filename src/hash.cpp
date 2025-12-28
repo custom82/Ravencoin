1222d9ae94a70a3218375d5fa7755c30581b6f2a
Andrea Postiglione <62989759+custom82@users.noreply.github.com>
2025-12-28 16:19:39 +0100

    Silence fallthrough warning in MurmurHash3

--- src/hash.cpp
+++ src/hash.cpp
@@ -55,8 +55,10 @@ unsigned int MurmurHash3(unsigned int nHashSeed, const std::vector<unsigned char
     switch (vDataToHash.size() & 3) {
         case 3:
             k1 ^= tail[2] << 16;
+            [[fallthrough]];
         case 2:
             k1 ^= tail[1] << 8;
+            [[fallthrough]];
         case 1:
             k1 ^= tail[0];
             k1 *= c1;
@@ -288,7 +290,6 @@ uint256 KAWPOWHash_OnlyMix(const CBlockHeader& blockHeader)
 
     return uint256S(to_hex(result));
 }
-
 
 
 
